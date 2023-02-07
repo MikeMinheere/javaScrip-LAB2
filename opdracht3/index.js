59db64486bf82b4fa256cbeead73aac33f90ea1d
MikeMinheere <99067548@mydavinci.nl>
2023-02-07 14:19:27 +0100

    Opdracht 3 af

---
+++ opdracht3/index.js
@@ -0,0 +1,39 @@
+var arrayEen = [1,2,3,4,5,6,7,8,9,10];
+var arrayTwee = [2,4,6,8,10,12,14,16,18,20];
+
+function arraySum(){
+    var sum = []
+    for(i = 0; i < arrayEen.length; i++){
+        sum.push(arrayEen[i]+arrayTwee[i])
+    }
+    return sum;
+}   
+
+function arrayMinus(){
+    var sum = []
+    for(i = 0; i < arrayEen.length; i++){
+        sum.push(arrayTwee[i]-arrayEen[i])
+    }
+    return sum;
+}   
+
+function arrayDivision(){
+    var sum = []
+    for(i = 0; i < arrayEen.length; i++){
+        sum.push(arrayTwee[i]/arrayEen[i])
+    }
+    return sum;
+}   
+
+function arrayMultiply(){
+    var sum = []
+    for(i = 0; i < arrayEen.length; i++){
+        sum.push(arrayEen[i]*arrayTwee[i])
+    }
+    return sum;
+}   
+
+document.write(arraySum());
+document.write(arrayMinus());
+document.write(arrayDivision());
+document.write(arrayMultiply());
