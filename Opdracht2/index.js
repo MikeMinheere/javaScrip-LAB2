073746a0e19aadb0555453d6d0babdee5166f4ca
MikeMinheere <99067548@mydavinci.nl>
2023-02-07 13:22:27 +0100

    Opdracht 2 af

--- Opdracht2/index.js
+++ Opdracht2/index.js
@@ -1,14 +1,19 @@
 var nameArray = [];
+var nameArrayReverse = [];
 document.getElementById("add").onclick = function(){
     nameArray.push(document.getElementById("name").value);
+    nameArrayReverse.push(document.getElementById("name").value);
 }
 
 document.getElementById("submit").onclick = function(){
     if(nameArray.length >= 3){
         document.getElementById("names").innerHTML = nameArray;
+        document.getElementById("namesReverse").innerHTML = nameArrayReverse.reverse();
+        
     }
     else{
         document.getElementById("names").innerHTML = "Je moet minimaal 3 namen typen";
+        document.getElementById("namesReverse").innerHTML = "Je moet minimaal 3 namen typen";
     }
 }
 
